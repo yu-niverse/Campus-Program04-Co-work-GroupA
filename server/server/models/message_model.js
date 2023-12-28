2c4adc2cfabcfa54ccebf9d95c82b8570f320434
raamiiChu <ericchu0617@gmail.com>
2023-12-28 16:36:04 +0800

    Fix: order by id

--- server/server/models/message_model.js
+++ server/server/models/message_model.js
@@ -1,6 +1,5 @@
 const { pool } = require('./mysqlcon');
 
-
 /*
 CREATE TABLE messages (
     message_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -14,26 +13,23 @@ CREATE TABLE messages (
 
 const createMessage = async (data) => {
   const { customer_id, message, time, sender_role } = data;
-  console.log("t", time);
-  const [result] = await pool.query('INSERT INTO messages (customer_id, message, sender_role, time) VALUES (?, ?, ?, ?)',
-    [customer_id, message, sender_role, time]);
+  console.log('t', time);
+  const [result] = await pool.query('INSERT INTO messages (customer_id, message, sender_role, time) VALUES (?, ?, ?, ?)', [customer_id, message, sender_role, time]);
   return result.insertId;
-}
+};
 
 const getMessages = async (userId, paging, pageSize) => {
-  const messageQuery = 'SELECT * FROM messages WHERE customer_id = ? ORDER BY time DESC LIMIT ?, ?';
+  const messageQuery = 'SELECT * FROM messages WHERE customer_id = ? ORDER BY message_id DESC LIMIT ?, ?';
 
-  const [messages] = await pool.query(messageQuery,
-    [userId, pageSize * paging, pageSize]);
+  const [messages] = await pool.query(messageQuery, [userId, pageSize * paging, pageSize]);
 
   return {
     messages,
-    messagesCount: messages.length
+    messagesCount: messages.length,
   };
-}
-
+};
 
 module.exports = {
   createMessage,
-  getMessages
-}
+  getMessages,
+};
