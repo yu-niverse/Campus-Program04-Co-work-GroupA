a16e910e53d869003f21c22fe748ae1066c45908
sandy-git-only <lihsuan.chen12@gmail.com>
2023-12-29 14:46:11 +0800

    fix: update stock bug

--- server/server/models/seckill_model.js
+++ server/server/models/seckill_model.js
@@ -19,40 +19,33 @@ async function buyProduct(productId, userId, quantity) {
     console.log('quantity: ' + quantity);
     const multi = redis.multi();
     // Start the transaction
-    multi.get(inventoryKey);
-
-    multi.set(userKey, 'true');
-
+    await multi.get(inventoryKey);
+    await multi.set(userKey, 'true');
     // Execute the transaction
     const results = await multi.exec();
-
     // Check if the inventory was greater than 0 before decrementing
-    const inventoryBeforeDecrement = parseInt(results[0][1], 10);
-    console.log('inventoryBeforeDecrement: ' + inventoryBeforeDecrement);
-    if (inventoryBeforeDecrement >= quantity) {
-        // Success, store user purchase information in Redis
+    const stock = await parseInt(results[0][1], 10);
+    console.log('inventoryBeforeDecrement: ' + stock);
+    if (stock >= quantity) {
+        // store user purchase information in Redis
         const USER_PURCHASE_PREFIX = 'user';
         const userPurchaseKey = `${USER_PURCHASE_PREFIX}:${userId}:product:${productId}`;
-        redis.set(userPurchaseKey, {
+        await redis.set(userPurchaseKey, {
             productId,
             userId,
-            quantity,
-            timestamp: Date.now(),
         });
-        multi.decrby(inventoryKey, quantity);
+        await multi.decrby(inventoryKey, quantity);
         return quantity;
     } else {
-        return 0; // Failure, not enough inventory
+        return 0; // not enough inventory
     }
 };
 
 async function syncPurchaseDataToDB() {
     const interval = 5000;
 
-    // 定義同步函數
     const syncFunction = async () => {
         const USER_PURCHASE_PREFIX = 'user';
-        // 查询 Redis 中的所有用户搶購信息
         const keys = await redis.keys(`${USER_PURCHASE_PREFIX}:*`);
         const pipeline = redis.pipeline();
         const connection = await pool.getConnection();
@@ -60,25 +53,31 @@ async function syncPurchaseDataToDB() {
         let productId
         for (const key of keys) {
             try {
-                await pipeline.hgetall(key);  // 使用 await 等待 pipeline 完成
+                await pipeline.hgetall(key); 
                 const match = key.match(/user:(\d+):product:(\d+)/);
+                const userId = match[1];
+                const productId = match[2];
                 if (match) {
-                    const userId = match[1];
-                    const productId = match[2];
-
                     const checkOrderQuery = 'SELECT * FROM order_seckills WHERE userId = ? AND productId = ?';
                     const [existingOrder] = await connection.execute(checkOrderQuery, [userId, productId]);
-                    if (!existingOrder.length)  {
+                    console.log("existingOrder", userId, productId,existingOrder.length);
+                    if (existingOrder.length < 1)  {
                         await connection.beginTransaction();
-                        const insertQuery = 'INSERT INTO order_seckills (userId, productId) VALUES (?, ?)';
+                        const insertQuery = 'INSERT IGNORE INTO order_seckills (userId, productId) VALUES (?, ?)';
                         // console.log("insert to order_seckills", userId,productId);
                         await connection.execute(insertQuery, [userId, productId]);
                         // await redis.del(key);
                         await connection.commit();
                     }
-
                     
-                }
+                };
+                // const inventoryKey = `product:${productId}:inventory`;
+                // const currentStock = await redis.get(inventoryKey);
+                // console.log("currentStock", currentStock, productId);
+                // if (productId != null){
+                //     const updateStockQuery = 'UPDATE seckill_variants SET stock = ? WHERE product_id = ?';
+                //     await connection.execute(updateStockQuery, [currentStock, productId]);
+                // };
             } catch (error) {
                 await connection.rollback();
                 throw error;
@@ -87,7 +86,6 @@ async function syncPurchaseDataToDB() {
             }
         }
 
-        // 等待當前的迴圈操作完成後再進行下一個步驟
         for (const key of keys) {
             const match = key.match(/user:(\d+):product:(\d+)/);
             if (match) {
@@ -95,17 +93,16 @@ async function syncPurchaseDataToDB() {
                 const inventoryKey = `product:${productId}:inventory`;
                 currentStock = await redis.get(inventoryKey);
                 console.log("currentStock", currentStock, productId);
-                // 在這裡處理 currentStock
+                if (productId != null){
+                    const updateStockQuery = 'UPDATE seckill_variants SET stock = ? WHERE product_id = ?';
+                    await connection.execute(updateStockQuery, [currentStock, productId]);
+                }
             }
         }
-        if (productId != null){
-            const updateStockQuery = 'UPDATE seckill_variants SET stock = ? WHERE product_id = ?';
-            await connection.execute(updateStockQuery, [currentStock, productId]);
-        }
+        
         
     };
 
-    // 開始定期執行同步函數
     setInterval(syncFunction, interval);
 };
 
@@ -121,15 +118,14 @@ async function syncProductInventoryToRedis(productId) {
     }
 }
 
-// 在需要商品庫存時，先從 Redis 中取得，如果沒有再同步庫存到 Redis
 async function getProductInventory(productId) {
     const inventoryKey = `product:${productId}:inventory`;
     let stock = await redis.get(inventoryKey);
-    if (stock === null) {
-        // 如果 Redis 中沒有庫存，則同步庫存到 Redis
+    console.log(stock)
+    if (!stock || stock == null) {
         await syncProductInventoryToRedis(productId);
-        // 再從 Redis 中取得庫存
         stock = await redis.get(inventoryKey);
+        
     }
 
     return stock;
