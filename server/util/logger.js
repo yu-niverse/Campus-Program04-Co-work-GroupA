947110b86bea19ef48a4e669f6ad6548140e755c
yu-niverse <chiehyu.cs09@nycu.edu.tw>
2023-12-31 17:20:41 +0800

    fix: fix logger

--- server/util/logger.js
+++ server/util/logger.js
@@ -33,22 +33,3 @@ const loggerStream = {
 };
 
 module.exports = { logger, loggerStream };
-
-
-// const { format, createLogger, transports } = require("winston");
-
-// const { combine, timestamp, label, printf } = format;
-// const CATEGORY = "winston custom format";
-
-// //Using the printf format.
-// const customFormat = printf(({ level, message, label, timestamp }) => {
-//   return `${timestamp} [${label}] ${level}: ${message}`;
-// });
-
-// const logger = createLogger({
-//   level: "debug",
-//   format: combine(label({ label: CATEGORY }), timestamp(), customFormat),
-//   transports: [new transports.Console()],
-// });
-
-// module.exports = logger;
